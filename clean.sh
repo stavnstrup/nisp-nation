69113db6ee2687c46a4918e747de2bcd2f1bbdb4
Jens Stavnstrup <stavnstrup@gmail.com>
2019-06-16 20:25:07 +0200

    Add script to remove generated files

---
+++ clean.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+rm -fR _capabilityprofile
+rm -fR _node
+rm -fR _organization
+rm -fR _profile
+rm -fR _responsibleparty
+rm -fR _serviceprofile
+rm -fR _standard
+rm _includes/graph*.html
+rm _includes/page*.html
+rm _includes/taxonomy.html
+rm -fR _site
+rm -fR _data
